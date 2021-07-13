760a7e7b3125f54ce12f660a287464346ee09e9c
CantusLupus <i.s.work234@gmail.com>
2021-07-13 23:17:35 +0200

    added 'add task' feature and actual tasks display

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
-from enum import unique
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Cantus:2345@localhost/ToDo'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://Cantus:2345@localhost/ToDo"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class ToDo(db.Model):
-  # __tablename__ = "ToDo"
+  __tablename__ = "ToDoTable"
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(50), unique=True)
-  email = db.Column(db.String(50), unique=True)
+  content = db.Column(db.String(200), nullable = False)
+  date_created = db.Column(db.DateTime, default=datetime.now)
 
-  def __init__(self, username, email):
-      self.username = username
-      self.email = email
+  def __repr__(self):
+      return "<Task %r>" % self.id
 
-
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+      task_content = request.form["content"]
+      new_task = ToDo(content = task_content)
+
+      try:
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect("/")
+      except:
+        return "The task has not been added"
+
+    else:
+      tasks = ToDo.query.order_by(ToDo.date_created).all()
+      return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
